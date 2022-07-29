ed953aa8c0837c3bfddfd8a798a544c95dd08d79
KwonYeKyeong <65434196+KwonYeKyeong@users.noreply.github.com>
2022-07-29 21:19:27 +0900

    settings : Exception 기능 정의 (#2) (#3)
    * feat : ErrorResponse 추가 (#2)
    
    * feat : BusinessException 클래스 정의 (#2)
    
    * feat : 예외처리 핸들러 추가 (#2)
    
    * style : 불필요한 공백 제거 (#2)

---
+++ src/main/java/umc/server/baeksstreetmapserver/common/error/exception/ErrorCode.java
@@ -0,0 +1,30 @@
+package umc.server.baeksstreetmapserver.common.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+	// Common
+	INVALID_INPUT_VALUE(400, "Invalid Input Value"),
+	METHOD_NOT_ALLOWED(405, "Invalid Input Value"),
+	ENTITY_NOT_FOUND(400, "Entity Not Found"),
+	INTERNAL_SERVER_ERROR(500, "Server Error"),
+	INVALID_TYPE_VALUE(400, " Invalid Type Value"),
+	HANDLE_ACCESS_DENIED(403, "Access is Denied"),
+
+	// User
+	EMAIL_DUPLICATION(400, "Email is Duplication"),
+	LOGIN_INPUT_INVALID(400, "Login input is invalid");
+
+	private final String message;
+	private int status;
+
+	ErrorCode(final int status, final String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+}
