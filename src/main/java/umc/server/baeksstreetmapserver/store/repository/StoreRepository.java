ebb30823d97f47190cb109eaf9048ee0278da7fd
yekyeong <kwonliz6.6@gmail.com>
2022-07-30 22:41:52 +0900

    feat : 바운더리 내 식당 조회 api 비즈니스 로직 작성 (#4)

--- src/main/java/umc/server/baeksstreetmapserver/store/repository/StoreRepository.java
+++ src/main/java/umc/server/baeksstreetmapserver/store/repository/StoreRepository.java
@@ -1,7 +1,16 @@
 package umc.server.baeksstreetmapserver.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import umc.server.baeksstreetmapserver.store.entity.Store;
 
+import java.util.List;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+	@Query(value = "SELECT s FROM Store s " +
+		"WHERE (s.latitude BETWEEN :startLatitude AND :endLatitude) " +
+		"AND (s.longitude BETWEEN :startLongitude AND :endLongitude)")
+	List<Store> findByBoundary(double startLatitude, double endLatitude, double startLongitude, double endLongitude);
+
 }
