ea0134d1f20698d6eebf3e1afbba64490e02ce03
sangwoonoel <77473684+sangwoonoel@users.noreply.github.com>
2022-08-08 09:55:26 +0900

    feat : user 도메인 entity 생성 (#9) (#10)
    * feat : ìuser 도메인 entity 생성 (#9)
    
    * refactor : password, image and nickname setter 작성, loginId, email, nickname 에 unique = true 조건추가  (#9)

---
+++ src/main/java/umc/server/baeksstreetmapserver/user/entity/User.java
@@ -0,0 +1,55 @@
+package umc.server.baeksstreetmapserver.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.server.baeksstreetmapserver.common.BaseEntity;
+import umc.server.baeksstreetmapserver.common.Status;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User extends BaseEntity {
+
+    private static final int MAX_NAME_LENGTH = 30;
+    private static final int MAX_PASSWORD_LENGTH = 30;
+    private static final int MAX_ID_LENGTH = 20;
+
+    @Id
+    @Column(name = "idx")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "login_id", nullable = false, length = MAX_ID_LENGTH,unique = true)
+    private String loginId;
+
+    @Column(name = "password", nullable = false, length = MAX_PASSWORD_LENGTH)
+    private String password;
+
+    @Column(name = "nickname", nullable = false, length = MAX_NAME_LENGTH, unique = true)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private Status status;
+
+    @Column(name = "profile_image")
+    private String image;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
