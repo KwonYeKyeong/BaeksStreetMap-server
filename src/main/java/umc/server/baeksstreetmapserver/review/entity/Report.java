e71bff5dd00ad5b07a9e4e2674646ae231aaa845
gyulimlee <91477376+gyulimlee@users.noreply.github.com>
2022-08-13 20:36:26 +0900

    feat : report entity 작성 (#16) (#17)
    * refactor : review entity converter import 수정 (#16)
    
    * feat : report entity 작성 (#16)

---
+++ src/main/java/umc/server/baeksstreetmapserver/review/entity/Report.java
@@ -0,0 +1,34 @@
+package umc.server.baeksstreetmapserver.review.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.server.baeksstreetmapserver.common.BaseEntity;
+import umc.server.baeksstreetmapserver.common.Status;
+import umc.server.baeksstreetmapserver.review.entity.Review;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Report extends BaseEntity {
+
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column(nullable = false)
+    private Long reason;
+
+    @Enumerated(EnumType.STRING)
+    @Column( nullable = false)
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_idx", nullable = false)
+    private Review review;
+
+}
