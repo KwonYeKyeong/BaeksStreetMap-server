530d8aa93b8ef81865486e36f013c32bfefbc08c
gyulimlee <91477376+gyulimlee@users.noreply.github.com>
2022-08-17 11:06:46 +0900

    feat : 리뷰 등록 api 키워드 추가 (#22) (#31)

---
+++ src/main/java/umc/server/baeksstreetmapserver/review/dto/RegisterReviewRequest.java
@@ -0,0 +1,54 @@
+package umc.server.baeksstreetmapserver.review.dto;
+
+
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.server.baeksstreetmapserver.common.Status;
+import umc.server.baeksstreetmapserver.review.entity.Keyword;
+import umc.server.baeksstreetmapserver.review.entity.Review;
+import umc.server.baeksstreetmapserver.review.entity.ReviewKeyword;
+import umc.server.baeksstreetmapserver.store.entity.Menu;
+import umc.server.baeksstreetmapserver.store.entity.Store;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+
+public class RegisterReviewRequest {
+
+    private List<String> reviewImgUrl;
+    private String reviewText;
+    private String like;
+    private Long change;
+    private List<Long> keyword;
+    private Long bestMenu;
+
+
+    @Builder
+    public RegisterReviewRequest(List<String> reviewImgUrl, String reviewText, String like, Long change, Long bestMenu, List<Long> keyword) {
+        this.reviewImgUrl = reviewImgUrl;
+        this.reviewText = reviewText;
+        this.like = like;
+        this.change = change;
+        this.keyword = keyword;
+        this.bestMenu = bestMenu;
+    }
+
+    //dto -> entity
+    public Review reviewToEntity(Menu menu, Store store) {
+        return Review.builder()
+                .likes(like.equals("Y") ? true : false)
+                .changes(change)
+                .text(reviewText)
+                .menu(menu)
+                .status(Status.ACTIVE)
+                .store(store)
+                .build();
+
+    }
+}
