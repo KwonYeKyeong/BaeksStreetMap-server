7dc9ebbb24823897b74a947ff609476f5b0372e8
yekyeong <kwonliz6.6@gmail.com>
2022-08-18 00:31:36 +0900

    feat : 로그인 아이디 중복확인 api 개발 (#33)

---
+++ src/main/java/umc/server/baeksstreetmapserver/user/dto/response/LoginIdDuplicateCheckResponse.java
@@ -0,0 +1,21 @@
+package umc.server.baeksstreetmapserver.user.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class LoginIdDuplicateCheckResponse {
+
+	private static final String DUPLICATED_MESSAGE = "중복된 아이디입니다.";
+	private static final String NOT_DUPLICATED_MESSAGE = "사용 가능한 아이디입니다.";
+
+	private String result;
+
+	private LoginIdDuplicateCheckResponse(String result) {
+		this.result = result;
+	}
+
+	public static LoginIdDuplicateCheckResponse of(boolean duplicatedOrNot){
+		return new LoginIdDuplicateCheckResponse(duplicatedOrNot == true ? DUPLICATED_MESSAGE : NOT_DUPLICATED_MESSAGE);
+	}
+
+}
---
+++ src/main/java/umc/server/baeksstreetmapserver/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package umc.server.baeksstreetmapserver.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import umc.server.baeksstreetmapserver.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	boolean existsByLoginId(String loginId);
+
+}
