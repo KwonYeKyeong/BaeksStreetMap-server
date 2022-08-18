baa1f25f9d575b8711399714eb4eca88fcf14b1d
yekyeong <kwonliz6.6@gmail.com>
2022-08-18 23:50:04 +0900

    refactor : request validation 추가 (#13)

--- src/main/java/umc/server/baeksstreetmapserver/user/dto/request/PatchNicknameReq.java
+++ src/main/java/umc/server/baeksstreetmapserver/user/dto/request/PatchNicknameReq.java
@@ -3,10 +3,14 @@ package umc.server.baeksstreetmapserver.user.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class PatchNicknameReq {
+
+    @Length(min = 1, max = 10)
     private String nickname;
+
 }
--- src/main/java/umc/server/baeksstreetmapserver/user/dto/request/PatchPasswordReq.java
+++ src/main/java/umc/server/baeksstreetmapserver/user/dto/request/PatchPasswordReq.java
@@ -3,11 +3,16 @@ package umc.server.baeksstreetmapserver.user.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class PatchPasswordReq {
+
     private String password;
+
+    @Length(min = 4)
     private String newPassword;
+
 }
